a51a27f695b71a801f0383a5c51bb9bbaca46400
MadhaviRockandla <rokandla@gmail.com>
2022-03-05 15:55:39 -0500

    finally completed

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -34,12 +34,16 @@ public class Board {
 
     public Boolean isHorizontalOf(Character player) {
 
-        boolean isHorizontalFound;
-        isHorizontalFound = false;
-        for (Character[] characters : matrix) {
-            if (characters[0] == player && characters[1] == player && characters[2] == player) {
-                isHorizontalFound = true;
-                break;
+        boolean isHorizontalFound = false;
+
+        //for (int i = 0; i < matrix.length; i++) {
+          //  if (matrix[0][i] == player && matrix[1][i] == player && matrix[2][i] == player) {
+            //    isHorizontalFound = true;
+              //  break;
+       for (Character[] characters : matrix) {
+           if (characters[0] == player && characters[1] == player && characters[2] == player) {
+               isHorizontalFound = true;
+              break;
             }
         }
         return isHorizontalFound;
